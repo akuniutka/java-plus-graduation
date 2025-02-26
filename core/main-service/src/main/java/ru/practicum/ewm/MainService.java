bf29ac8df3f900c45933c7933fe31f54739947be
Andrei Kuniutka <akuniutka@gmail.com>
2025-02-26 09:36:12 +0300

    Feat: Add Service Discovery, Externalized Configuration and API Gateway

--- core/main-service/src/main/java/ru/practicum/ewm/MainService.java
+++ core/main-service/src/main/java/ru/practicum/ewm/MainService.java
@@ -2,10 +2,12 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.time.Clock;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MainService {
 
