62543998f8945003f47512ca6930c1c2461c696d
Andrei Kuniutka <akuniutka@gmail.com>
2025-03-10 22:16:18 +0300

    Refactor: Copy common classes to each of microservices
    Copy classes shared by several microservices to that microservices
    and drop module 'common'. It allows to avoid indirect dependency of
    one microservice on others.

--- core/request-service/src/main/java/ru/practicum/ewm/exception/ControllerExceptionHandler.java
+++ core/request-service/src/main/java/ru/practicum/ewm/exception/ControllerExceptionHandler.java
@@ -1,28 +1,53 @@
 package ru.practicum.ewm.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
-class ControllerExceptionHandler extends BaseExceptionHandler {
+@RequiredArgsConstructor
+@Slf4j
+public class ControllerExceptionHandler {
 
-    ControllerExceptionHandler(final Clock clock) {
-        super(clock);
+    private final Clock clock;
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingQueryParameter(final MissingServletRequestParameterException exception) {
+        log.warn(exception.getMessage());
+        final List<FieldErrorData> errors = List.of(new FieldErrorData(exception.getParameterName(),
+                "no value provided", null));
+        return handleFieldErrorInternally(ParameterType.PARAMETER, errors);
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleWrongValuesInQueryString(
-            final HandlerMethodValidationException exception,
-            final HttpServletRequest httpRequest) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleWrongTypeOfQueryParameter(final MethodArgumentTypeMismatchException exception) {
+        log.warn(exception.getMessage());
+        final List<FieldErrorData> errors = List.of(new FieldErrorData(exception.getName(), "value of wrong type",
+                exception.getValue()));
+        return handleFieldErrorInternally(ParameterType.PARAMETER, errors);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleWrongValuesInQueryString(final HandlerMethodValidationException exception) {
         log.warn(exception.getMessage());
         final List<FieldErrorData> errors = exception.getValueResults().stream()
                 .flatMap(result -> result.getResolvableErrors().stream()
@@ -31,64 +56,133 @@ class ControllerExceptionHandler extends BaseExceptionHandler {
                                 error.getDefaultMessage(),
                                 result.getArgument())))
                 .toList();
-        return handleFieldErrorDataInternally(ParameterType.PARAMETER, errors, httpRequest);
+        return handleFieldErrorInternally(ParameterType.PARAMETER, errors);
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleFieldValidationException(
-            final FieldValidationException exception,
-            final HttpServletRequest httpRequest) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleParameterValidationException(final ParameterValidationException exception) {
+        log.warn(exception.getMessage());
+        final List<FieldErrorData> errors = List.of(new FieldErrorData(exception.getParameter(), exception.getError(),
+                exception.getValue()));
+        return handleFieldErrorInternally(ParameterType.PARAMETER, errors);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleWrongValuesInRequestBody(final MethodArgumentNotValidException exception) {
+        log.warn(exception.getMessage());
+        final List<FieldErrorData> errors = exception.getFieldErrors().stream()
+                .map(error -> new FieldErrorData(error.getField(), error.getDefaultMessage(), error.getRejectedValue()))
+                .toList();
+        return handleFieldErrorInternally(ParameterType.FIELD, errors);
+    }
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleFieldValidationException(final FieldValidationException exception) {
         log.warn(exception.getMessage());
         final List<FieldErrorData> errors = List.of(new FieldErrorData(exception.getField(), exception.getError(),
                 exception.getValue()));
-        return handleFieldErrorDataInternally(ParameterType.FIELD, errors, httpRequest);
+        return handleFieldErrorInternally(ParameterType.FIELD, errors);
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleDatabaseConstraintViolation(final DataIntegrityViolationException exception) {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDatabaseConstraintViolation(final DataIntegrityViolationException exception) {
         log.warn(exception.getMessage());
-        final ApiError apiError = ApiError.builder()
+        return ApiError.builder()
                 .status(HttpStatus.CONFLICT)
                 .reason("Integrity constraint has been violated")
                 .message(exception.getMessage())
                 .timestamp(LocalDateTime.now(clock))
                 .build();
-        return new ResponseEntity<>(apiError, apiError.status());
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleNotPossibleException(final NotPossibleException exception) {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleNotPossibleException(final NotPossibleException exception) {
         log.warn(exception.getMessage());
-        final ApiError apiError = ApiError.builder()
+        return ApiError.builder()
                 .status(HttpStatus.CONFLICT)
                 .reason("For the requested operation the conditions are not met")
                 .message(exception.getMessage())
                 .timestamp(LocalDateTime.now(clock))
                 .build();
-        return new ResponseEntity<>(apiError, apiError.status());
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleNotFoundException(final NotFoundException exception) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNotFoundException(final NotFoundException exception) {
         log.warn(exception.getMessage());
-        final ApiError apiError = ApiError.builder()
+        return ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .reason("The required object was not found")
                 .message(exception.getMessage())
                 .timestamp(LocalDateTime.now(clock))
                 .build();
-        return new ResponseEntity<>(apiError, apiError.status());
     }
 
     @ExceptionHandler
-    public ResponseEntity<Object> handleEventNotFoundException(final EventNotFoundException exception) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleEventNotFoundException(final EventNotFoundException exception) {
         log.warn(exception.getMessage());
-        final ApiError apiError = ApiError.builder()
+        return ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .reason("The required object was not found")
                 .message(exception.getMessage())
                 .timestamp(LocalDateTime.now(clock))
                 .build();
-        return new ResponseEntity<>(apiError, apiError.status());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleException(final Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .reason("Unexpected error")
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now(clock))
+                .build();
+    }
+
+    private ApiError handleFieldErrorInternally(final ParameterType parameterType, final List<FieldErrorData> errors) {
+        final List<FieldErrorData> orderedErrors = errors.stream()
+                .sorted(Comparator.comparing(FieldErrorData::field).thenComparing(FieldErrorData::error))
+                .toList();
+        final Set<String> fields = orderedErrors.stream()
+                .map(FieldErrorData::field)
+                .map("'%s'"::formatted)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        return ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .reason("Wrong request format")
+                .message(makeMessage(parameterType, fields, errors))
+                .errors(orderedErrors)
+                .timestamp(LocalDateTime.now(clock))
+                .build();
+    }
+
+    private String makeMessage(final ParameterType parameterType, final Set<String> fields,
+            final List<FieldErrorData> errors) {
+        final String type = parameterType == ParameterType.FIELD ? "field" : "parameter";
+        if (fields.size() == 1 && errors.size() == 1) {
+            return "There is an error in %s %s".formatted(type, fields.iterator().next());
+        } else if (fields.size() == 1) {
+            return "There are errors in %s %s".formatted(type, fields.iterator().next());
+        } else {
+            return "There are errors in %ss %s".formatted(type, String.join(", ", fields));
+        }
+    }
+
+    private record FieldErrorData(String field, String error, Object value) {
+
+    }
+
+    protected enum ParameterType {
+        PARAMETER,
+        FIELD
     }
 }
