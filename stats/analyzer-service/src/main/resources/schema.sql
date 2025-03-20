35957f1f1ebd808fddfa1333e323c67b98b8e3bd
Andrei Kuniutka <akuniutka@gmail.com>
2025-03-20 20:37:53 +0300

    Refactor: Add NOT NULL constraints to relations in analyzer-service

--- stats/analyzer-service/src/main/resources/schema.sql
+++ stats/analyzer-service/src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS user_scores
 (
   id        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  event_id  BIGINT,
-  user_id   BIGINT,
-  score     REAL,
-  timestamp TIMESTAMP
+  event_id  BIGINT    NOT NULL,
+  user_id   BIGINT    NOT NULL,
+  score     REAL      NOT NULL,
+  timestamp TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS similarity_scores
 (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  event_a_id BIGINT,
-  event_b_id BIGINT,
-  score REAL,
-  timestamp TIMESTAMP
+  id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  event_a_id BIGINT    NOT NULL,
+  event_b_id BIGINT    NOT NULL,
+  score      REAL      NOT NULL,
+  timestamp  TIMESTAMP NOT NULL
 );
